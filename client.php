39a755d39b5677251f4ecb80f5a62d68632b4f91
andrey-gthb <johnandreybulawan@gmail.com>
2025-09-06 23:47:27 +0800

    changes notice connect

--- client.php
+++ client.php
@@ -2,7 +2,7 @@
 session_start();
 require "db.php";
 
-// Redirect if not client
+// Redirect if not client changes notice!
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'client') {
     header("Location: login.php");
     exit();
